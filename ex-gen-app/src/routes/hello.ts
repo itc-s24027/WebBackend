5e49d9399d0e7650892b36ad22b399bca89c10bc
itc-s24027 <s24027@std.it-college.ac.jp>
2025-10-23 14:52:46 +0900

    IDを指定してデータを取ってくることができた

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -21,10 +21,36 @@ interface MyData {
 
 router.get('/', async (req, res, next) => {
     const result = await db.query<MyData[]>('SELECT * FROM mydata')
-    res.render('hello', {
+    res.render('hello/index', {
         title: 'Hello!',
         content: result,
     })
+})
+
+router.get('/add', async (req, res, next) => {
+    res.render('hello/add', {
+        title: 'Hello/Add!',
+        content: '新しいレコードを入力'
+    })
+})
+
+router.post('/add', async (req, res, next) => {
+    const {name, mail, age} = req.body
+    await db.query('INSERT INTO mydata (name, mail, age) VALUES (?, ?, ?)', [
+        name, mail, age
+    ])
+    res.redirect('/hello')
+})
+
+router.get('/show', async (req, res, next) => {
+    const id = Number(req.query.id)
+    const result: MyData[] = await db.query(
+        'SELECT * FROM mydata WHERE id = ?',[id])
+    res.render('hello/show', {
+        title: 'Hello!/show',
+        content: `id=${id}のレコード`,
+        mydata: result[0],
+        })
 })
 
 export default router
